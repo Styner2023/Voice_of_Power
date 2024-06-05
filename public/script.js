460373be86353554e564e3150128f899410b9463
Ubuntu <ubuntu@ip-172-31-13-207.us-west-1.compute.internal>
2024-06-05 09:07:56 +0000

    Updated script.js to display uploaded book message

--- public/script.js
+++ public/script.js
@@ -1,6 +1,16 @@
 document.getElementById('uploadBook').addEventListener('change', function(event) {
-    const bookView = document.getElementById('bookView');
-    bookView.innerHTML = '<p>Book uploaded and ready to be read aloud.</p>';
+    const file = event.target.files[0];
+    if (file && file.type === 'application/pdf') {
+        const fileReader = new FileReader();
+        fileReader.onload = function(e) {
+            const pdfData = e.target.result;
+            const pdfViewer = document.getElementById('pdfViewer');
+            pdfViewer.src = pdfData;
+        };
+        fileReader.readAsDataURL(file);
+    } else {
+        alert('Please upload a valid PDF file.');
+    }
 });
 
 document.getElementById('createAccount').addEventListener('click', function() {
